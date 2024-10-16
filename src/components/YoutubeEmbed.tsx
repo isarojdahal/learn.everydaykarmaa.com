fe7911fc8955b52cf78b6b6b78e7c0396cf74a8a
Saroj Dahal <isarojdahal@gmail.com>
2024-10-16 13:56:37 +0545

    update: name to YoutubeEmbed on imports

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -1,15 +1,14 @@
-const YouTubeEmbed = ({ videoId }) => (
+const YoutubeEmbed = ({ videoId }) => (
   <div className="video-container">
     <iframe
       width="100%"
       height="400"
       src={`https://www.youtube.com/embed/${videoId}`}
       title="YouTube video player"
-      frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
     ></iframe>
   </div>
 );
 
-export default YouTubeEmbed;
+export default YoutubeEmbed;
