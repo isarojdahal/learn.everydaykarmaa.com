e8f39bfd1c0a7f5d41b51fc3ad3b4e92b40899c8
Saroj Dahal <isarojdahal@gmail.com>
2024-10-16 13:15:27 +0545

    update: blog edit URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/isarojdahal/learn.everydaykarmaa.com/tree/develop/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
