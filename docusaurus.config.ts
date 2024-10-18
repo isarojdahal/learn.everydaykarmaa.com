e8343c0ed47561216a2bb0eee900e2729f2b10ef
Saroj Dahal <isarojdahal@gmail.com>
2024-10-18 14:42:13 +0545

    update: configs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "Welcome",
+  title: "Learn.everydaykarmaa",
   tagline: "Learn Everyday with EverydayKarma",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://learn.everydaykarmaa.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "EverydayKarma", // Usually your GitHub org/user name.
+  projectName: "Learn.EverydayKarma", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
